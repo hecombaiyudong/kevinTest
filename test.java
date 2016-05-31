b43bb4125a5a81a5d96d8f1859eaac5d1f370d40
Kevin Bai <baiyudong@hecom.cn>
2016-05-31 10:03:48 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+public static void startAddWithCustomer(Activity activity, String customCode,
+                                            String customName) {
+        if (TextUtils.isEmpty(customCode)) {
+            throw new IllegalArgumentException("customCode could not be null");
+        }
+        if (TextUtils.isEmpty(customName)) {
+            throw new IllegalArgumentException("customName could not be null");
+        }
+        Intent intent = new Intent(activity, CustomContactAddEditActivity.class);
+        intent.putExtra(Mode, ADD_WITH_CUSTOM);
+        intent.putExtra(CUSTOM_CODE, customCode);
+        intent.putExtra(CUSTOM_NAME, customName);
+        activity.startActivityForResult(intent, CONTACT_CHANGE);
+    }
